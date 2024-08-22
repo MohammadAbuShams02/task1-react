2d00ac52f0b595b73dfd7cb3b2895435b0d56f6a
MohammadAbuShams <1200549@student.birzeit.edu>
2024-08-22 23:40:16 +0300

    task1

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from './Navbar';
+import Employees from './Employees';
+import Clients from './Clients';
+import Footer from './Footer';
+
+export default function App() {
+  return (
+    <div>
+     
+     <Navbar />
+      <Employees />
+      <Clients />
+      <Footer />
+    </div>
+  );
+}
